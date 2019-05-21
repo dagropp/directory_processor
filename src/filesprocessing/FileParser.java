18366772b71870a15ac90c4af0c2d1bc5966ddb2
dagropp <X5rrcfbr>
2019-05-21 13:48:43 +0300

    2019-05-21 - Version 0: Created basic package and classes. Still not done creating all the necessary classes.

---
+++ src/filesprocessing/FileParser.java
@@ -0,0 +1,12 @@
+package filesprocessing;
+
+import java.io.*;
+
+/**
+ * Receives file and parse each line to String array.
+ */
+public class FileParser {
+    public FileParser(File file) {
+
+    }
+}
